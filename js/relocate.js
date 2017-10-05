0309af3f135dc5f603f33473e5a93f25a610c425
Romain Lefort <rlefort@beapi.fr>
2017-10-05 10:35:51 +0200

    Init commit

---
+++ js/relocate.js
@@ -0,0 +1,31 @@
+(function (window, document, undefined) {
+	'use strict';
+
+  // Initialize the media query
+  var bp = 1024
+	var mediaQuery = window.matchMedia(`(min-width: ${bp}px)`)
+
+	// Add a listen event
+	mediaQuery.addListener(initRelocate)
+
+	// Function to do something with the media query
+	function initRelocate (mediaQuery) {
+		if (mediaQuery.matches) {
+      reloacteFoo('dekstop')
+		} else {
+      reloacteFoo('mobile')
+		}
+  }
+
+  function reloacteFoo (match) {
+    if (match === 'desktop') {
+      // Do something for desktop breakpoint
+    } else if (match === 'mobile') {
+      // Do something for mobile breakpoint
+    }
+  }
+
+	// On load
+	initRelocate(mediaQuery);
+
+})(window, document);
