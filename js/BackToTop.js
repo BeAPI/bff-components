b43c2539b4cf6209ad4ac6fdf994f61c8496b688
Milan Ricoul <mricoul@beapi.fr>
2018-12-18 11:26:20 +0100

    add back to top component

---
+++ js/BackToTop.js
@@ -0,0 +1,40 @@
+class BackToTop {
+  constructor(offset = 100, duration = 300) {
+    this.offset = offset
+    this.duration = duration
+  }
+  init() {
+    this.buildBackToTopButton()
+    this.toggleBackToTopButton()
+
+    window.addEventListener('scroll', () => this.toggleBackToTopButton())
+  }
+  /**
+   * Append back to top button before closed body tag
+   */
+  buildBackToTopButton() {
+    const backToTopButton = document.createElement('button')
+    backToTopButton.classList.add('back-to-top', 'button', 'button--icon')
+    backToTopButton.setAttribute('id', 'js-back-to-top')
+    backToTopButton.setAttribute('data-goto', '0')
+    backToTopButton.innerHTML =
+      '<svg class="icon icon-up" aria-hidden="true" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-up"></use></svg>'
+
+    document.body.appendChild(backToTopButton)
+  }
+  /**
+   * Hide & show back to top button
+   */
+  toggleBackToTopButton() {
+    const backToTopButton = document.getElementById('js-back-to-top')
+
+    window.pageYOffset > this.offset
+      ? backToTopButton.classList.remove('back-to-top--hidden')
+      : backToTopButton.classList.add('back-to-top--hidden')
+  }
+}
+
+export default BackToTop
+
+const backToTop = new BackToTop()
+backToTop.init()
