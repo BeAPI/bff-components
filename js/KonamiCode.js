6abb90aa7c3168e4b7535e7f90c7a458528eab7b
Milan Ricoul <mricoul@beapi.fr>
2019-02-20 00:13:55 +0100

    remove jquery on KonamiCode

--- js/KonamiCode.js
+++ js/KonamiCode.js
@@ -1,21 +1,18 @@
-const $ = require('jquery')
-
 const k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let n = 0
 
-$(document).keydown(e => {
+const foo = () => {
+  console.log('Konami code !')
+}
+
+document.addEventListener('keydown', e => {
   if (e.keyCode === k[n++]) {
     if (n === k.length) {
       foo()
       n = 0
       return false
     }
-  }
-  else {
+  } else {
     n = 0
   }
-});
-
-function foo () {
-  console.log('Konami code !')
-}
+})
